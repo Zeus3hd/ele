6ceefa2facae70569a1ee18d78c392ef7f5102e0
Ahd <dafuq.ahd@gmail.com>
2020-04-06 16:21:14 +0300

    redirect on form submit

--- src/components/layout/PostProduct/index.js
+++ src/components/layout/PostProduct/index.js
@@ -1,25 +1,11 @@
 import React, { useState } from "react";
 import { ProductForm, TextInput, SubmitButton, FileField } from "./index.style";
+import { Redirect } from "react-router-dom";
 const PostProduct = () => {
   const [name, setName] = useState("");
   const [productType, setProductType] = useState("");
   const [img, setImg] = useState();
-  const newPost = () => {
-    const uploadData = new FormData();
-    uploadData.append("name", name);
-    uploadData.append("product_type", productType);
-    uploadData.append("img", img);
-    console.log(uploadData);
-    fetch("/api/product/", {
-      mode: "no-cors",
-      method: "POST",
-      body: uploadData,
-    })
-      .then((res) => {
-        console.log("uplaoded");
-      })
-      .catch((e) => console.log(e));
-  };
+  const [redirect, setRedirect] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     const uploadData = new FormData();
@@ -33,25 +19,30 @@ const PostProduct = () => {
     })
       .then((res) => {
         console.log("uplaoded");
+        setRedirect(true);
       })
       .catch((e) => console.log(e));
   };
-  return (
-    <ProductForm>
-      <TextInput
-        placeholder="title"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <TextInput
-        placeholder="product type"
-        value={productType}
-        onChange={(e) => setProductType(e.target.value)}
-      />
-      <FileField type="file" onChange={(e) => setImg(e.target.files[0])} />
-      <SubmitButton onClick={handleSubmit}>Post</SubmitButton>
-    </ProductForm>
-  );
+  if (redirect) {
+    return <Redirect to="/gallery" />;
+  } else {
+    return (
+      <ProductForm>
+        <TextInput
+          placeholder="title"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextInput
+          placeholder="product type"
+          value={productType}
+          onChange={(e) => setProductType(e.target.value)}
+        />
+        <FileField type="file" onChange={(e) => setImg(e.target.files[0])} />
+        <SubmitButton onClick={handleSubmit}>Post</SubmitButton>
+      </ProductForm>
+    );
+  }
 };
 
 export default PostProduct;
